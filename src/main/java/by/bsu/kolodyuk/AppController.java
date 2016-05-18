7b8befad716976d91c3febfed3cbc42facd50982
Kaladziuk, Stanislau <stanislau.kaladziuk@ihg.com>
2016-05-18 22:20:12 +0300

    Implement Recognizer

--- src/main/java/by/bsu/kolodyuk/AppController.java
+++ src/main/java/by/bsu/kolodyuk/AppController.java
@@ -4,17 +4,17 @@ package by.bsu.kolodyuk;
 import by.bsu.kolodyuk.model.Example;
 import by.bsu.kolodyuk.model.Type;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class AppController implements Initializable {
 
@@ -22,6 +22,9 @@ public class AppController implements Initializable {
     private FlowPane propertiesPane;
     @FXML
     private AnchorPane answersPane;
+    @FXML
+    private ListView resultListView;
+    private ObservableList<String> resultData;
     private List<ToggleButton> toggleButtons;
 
     private List<Type> types;
@@ -34,12 +37,20 @@ public class AppController implements Initializable {
         learn();
         toggleButtons = new ArrayList<>();
         properties.forEach(p -> createToggleButton(p));
-
+        resultData = FXCollections.observableArrayList();
+        resultListView.setItems(resultData);
     }
 
     @FXML
     public void onRecognizeButtonPressed() {
         recognize();
+    }
+
+    @FXML
+    public void onTryAgainButtonPressed() {
+        toggleButtons.stream().forEach(b -> b.setSelected(false));
+        Arrays.fill(x, 0.0);
+        resultData.clear();
     }
 
     public void learn() {
@@ -54,7 +65,7 @@ public class AppController implements Initializable {
                 for(Example example : type.getExamples()) {
                     for(String property : example.getProperties()) {
                         int index = properties.indexOf(property);
-                        b[index] += 1.0 / types.size();
+                        b[index] += 1.0 / type.getExamples().size();
                     }
                 }
                 type.setMasses(b);
@@ -76,19 +87,26 @@ public class AppController implements Initializable {
                     a[i] = Math.abs(type.getMasses()[i] - b[i]);
                 }
                 type.setDeltas(a);
-                //System.out.println(Arrays.toString(a));
+                System.out.println(Arrays.toString(a));
             }
+
+            x = new double[properties.size()];
+
         } catch(IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void recognize() {
+        resultData.clear();
+
         double[] result = new double[types.size()];
+        String[] records = new String[types.size()];
 
         for(int i = 0; i < types.size(); i++) {
-            double sum = 0;
+            List<Double> sums = new ArrayList<>();
             for(Example example : types.get(i).getExamples()) {
+                double sum = 0;
                 for(int j = 0; j < properties.size(); j++) {
                     String property = properties.get(j);
                     double y = example.getProperties().contains(property) ? 1 : 0;
@@ -98,15 +116,26 @@ public class AppController implements Initializable {
                         sum -= types.get(i).getDeltas()[j];
                     }
                 }
+                sum /= Arrays.stream(types.get(i).getDeltas()).sum();
+                sum = Math.max(0, sum);
+                sums.add(sum);
             }
-            result[i] = Math.max(0, sum);
+
+            result[i] = Collections.max(sums);
+            records[i] = types.get(i).getExamples().get(sums.indexOf(result[i])).getName();
+            resultData.add(types.get(i).getName() + " > " + records[i] + " > " + result[i]);
         }
 
-        double max = Arrays.stream(result).max().getAsDouble();
+
         System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(records));
+
+        double max = Arrays.stream(result).max().getAsDouble();
         for(int i = 0; i < result.length; i++) {
             if(result[i] == max) {
                 System.out.println(types.get(i).getName());
+                resultListView.getSelectionModel().select(i);
+                resultListView.requestFocus();
             }
         }
     }
@@ -114,8 +143,8 @@ public class AppController implements Initializable {
     public ToggleButton createToggleButton(String property) {
         final ToggleButton button = new ToggleButton(property);
         propertiesPane.getChildren().add(button);
-        toggleButtons.add(button);
         int index = toggleButtons.size();
+        toggleButtons.add(button);
         button.setOnAction(e -> {
              x[index] = button.isSelected() ? 1 : 0;
         });
